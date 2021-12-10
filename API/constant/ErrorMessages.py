5e5e6ee895de379cf3c0748f6682509fc602318b
Dmitry Aratin <dima.aratin@mail.ru>
2021-12-10 16:48:02 +0300

    Append route and logic update country

--- API/constant/ErrorMessages.py
+++ API/constant/ErrorMessages.py
@@ -5,3 +5,5 @@ class ErrorMessages(object):
 
     REGISTRATION_ERROR_400 = "Данный пользователь уже зарегистрирован"
     LOGIN_ERROR_401 = "Пользователя с такими данными не существует"
+
+    COUNTRIES_NOT_FOUND = "Страны с такими параметрами не найдено"
