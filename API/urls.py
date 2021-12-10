afc1a57458378e93f812c9238d3a203354b3a6ef
Dmitry Aratin <dima.aratin@mail.ru>
2021-12-10 17:46:06 +0300

    Append delete history

--- API/urls.py
+++ API/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('api/countries', views.get_all_countries, name="get_all_countries"),
     path('api/countries/<int:id>', views.get_country, name="get_country"),
     path('api/userprofile/<int:user_profile_id>/country', views.update_country, name="update_country"),
-    path('api/userprofile/<int:user_profile_id>/histories', views.show_histories, name="show_histories")
+    path('api/userprofile/<int:user_profile_id>/histories', views.show_histories, name="show_histories"),
+    path('api/userprofile/<int:user_profile_id>/histories/<int:history_id>', views.delete_histories, name="delete_histories"),
 ]
 
 # включаем возможность обработки картинок
