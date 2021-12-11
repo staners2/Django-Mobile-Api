4f55be052f9a8b67840e3479efba51504d70d7b0
Dmitry Aratin <dima.aratin0@gmail.com>
2021-12-11 10:42:13 +0300

    Inside translate in api

--- API/Helpers.py
+++ API/Helpers.py
@@ -2,6 +2,8 @@ from translate import Translator
 
 class Helpers(object):
 
-    def translate_language(from_lnaguage, to_language, source):
+    def translate_language(from_lnaguage, to_language, source, status_translate=False):
         translator = Translator(from_lnag=from_lnaguage, to_lang=to_language)
+        if (status_translate == False):
+            return source
         return translator.translate(source)
