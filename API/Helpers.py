bc780bc8373410b64c6527d3a5ea990a19a2bc3f
Dmitriy Aratin <dima.aratin@mail.ru>
2022-01-03 22:38:29 +0700

    Append translate

--- API/Helpers.py
+++ API/Helpers.py
@@ -2,7 +2,7 @@ from translate import Translator
 
 class Helpers(object):
 
-    def translate_language(from_lnaguage, to_language, source, status_translate=False):
+    def translate_language(to_language, source, from_lnaguage = "en", status_translate=False):
         translator = Translator(from_lnag=from_lnaguage, to_lang=to_language)
         if (status_translate == False):
             return source
