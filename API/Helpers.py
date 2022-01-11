fca13d78f33860d1e7958b10bd031c6d58fe167a
Dmitriy Aratin <dima.aratin@mail.ru>
2022-01-11 14:33:56 +0700

    change language

--- API/Helpers.py
+++ API/Helpers.py
@@ -2,7 +2,7 @@ from translate import Translator
 
 class Helpers(object):
 
-    def translate_language(to_language, source, from_lnaguage = "en", status_translate=False):
+    def translate_language(to_language, source, from_lnaguage = "en", status_translate=True):
         translator = Translator(from_lnag=from_lnaguage, to_lang=to_language)
         if (status_translate == False):
             return source
